62f5794af56928d1fbc4e7f57d457888ee0aeef9
Nicolas Cordoba <nicolascordoba89@gmail.com>
2017-09-07 19:53:26 -0300

    header

---
+++ home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Proyecto</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://meyerweb.com/eric/tools/css/reset/reset.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="home.css">
+  </head>
+  <body>
+    <div class="container">
+      <header>
+      <h1>Cerveceria Artesanal</h1>
+      <div class="login">
+        <ul>
+          <li>Ingresar</li>
+          <li>Registro</li>
+        </ul>
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#">Nosotros</a></li>
+          <li><a href="#">Tienda</a></li>
+          <li><a href="#">Como trabajamos?</a></li>
+          <li><a href="#">Contacto</a></li>
+        </ul>
+      </nav>
+    </div>
+  </body>
+</html>
