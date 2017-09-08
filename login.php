3046282fbefc073a1f9e7ef919318f23d8cf6e4e
Nicolas Cordoba <nicolascordoba89@gmail.com>
2017-09-07 21:24:26 -0300

    subo login

---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ingresar</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://meyerweb.com/eric/tools/css/reset/reset.css">
+    <link rel="stylesheet" href="login.css">
+  </head>
+  <body>
+    <form action="" method="get">
+      <div class="container">
+        <h1>Ingresar</h1>
+        <ul>
+          <li>
+            <label>E-Mail</label>
+            <input type="text" name="user" required>
+          </li>
+          <li>
+            <label>Contraseña</label>
+            <input type="password" name="pass" placeholder="Contraseña" required>
+          </li>
+          <li><a href="#">Olvidaste tu contraseña? Recuperar</a></li>
+          <li><a href="#">Aun no tenes usuario? Registrarse</a></li>
+          <li>
+            <button type="submit">Loguearse</button>
+            <button type="button">Cancelar</button>
+          </li>
+          <li>
+            <input type="checkbox" checked>Recordarme
+          </li>
+        </ul>
+      </div>
+    </form>
+  </body>
+</html>
