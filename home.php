ceadbfbaa32d0cac76462ef6dcf9604c6df1996e
alvizu <robert.alvizu@gmail.com>
2017-09-10 17:40:29 -0300

    agregar jumbotron

--- home.php
+++ home.php
@@ -71,6 +71,11 @@
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
+<div class="jumbotron">
+  <h1>Hello, world!</h1>
+  <p>...</p>
+  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+</div>
     </div>
     <script src="js/jquery-3.2.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
