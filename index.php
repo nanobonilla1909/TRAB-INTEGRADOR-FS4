bc34773e5de6efdb0bd82f9e263f4d9719a2a753
Mariano Bonilla <mariano_bonilla1909@hotmail.com>
2017-09-15 16:04:25 -0300

    Cambio de encabezado y Nav

--- index.php
+++ index.php
@@ -6,15 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://meyerweb.com/eric/tools/css/reset/reset.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
     <div class="container">
       <header>
-        <h1><a href="index.php"><img src="" alt="">Logo</a></h1>
+        <h1 class="logo"><a href="index.php"><img src="" alt=""><img src="img/logo1.png" alt=""></a></h1>
+        <form class="buscador">
+          <div>
+            <!-- <input type="text" class="form-control placeholder="Busque aqui su producto">
+            <button type="submit" class="btn btn-default">Buscar</button> -->
+            <input type="text" class="form-controlx placeholder="Buscar">
+            <button type="submit" class="btn btn-default">Buscar</button>
+          </div>
+        </form>
         <div class="login">
           <ul>
+            <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a></li>
             <li><a href="login.php">Ingresar</a></li>
+            <li><span> | </span></li>
             <li><a href="register.php">Registrarse</a></li>
           </ul>
         </div>
@@ -34,10 +44,6 @@
           <!-- Collect the nav links, forms, and other content for toggling -->
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-              <li class="active"><a href="#">Quienes somos <span class="sr-only">(current)</span></a>
-              </li>
-              <li><a href="#">Promos</a>
-              </li>
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Variedades <span class="caret"></span></a>
                 <ul class="dropdown-menu">
@@ -54,13 +60,19 @@
                   <li><a href="#">Importadas</a></li>
                 </ul>
               </li>
+              <li><a href="#">Promos</a>
+              </li>
+              <li><a href="#">Quienes somos <span class="sr-only">(current)</span></a>
+              </li>
+              <li><a href="faqs.php">Preguntas Frecuentes</a>
+              </li>
             </ul>
-            <form class="navbar-form navbar-left">
+            <!-- <form class="navbar-form navbar-left">
               <div class="form-group">
                 <input type="text" class="form-control" placeholder="Search">
               </div>
               <button type="submit" class="btn btn-default">Buscar</button>
-            </form>
+            </form> -->
             <ul class="nav navbar-nav navbar-right">
               <li><a href="car.php">Carrito</a></li>
               <li class="dropdown">
@@ -88,7 +100,7 @@
         <div class="row">
           <div class="col-sm-6 col-md-3">
             <div class="thumbnail">
-              <img src="..." alt="...">
+              <img src="img/golden.jpg" alt="...">
               <div class="caption">
                 <h3>⁠⁠⁠Golden</h3>
                 <p>Es lo más parecido a una cerveza clásica de supermercado que vas a encontrar en el mundo artesanal. Suave, de amargor y alcohol bajos.</p>
@@ -98,7 +110,7 @@
           </div>
           <div class="col-sm-6 col-md-3">
             <div class="thumbnail">
-              <img src="..." alt="...">
+              <img src="img/pale-ale.jpeg" alt="...">
               <div class="caption">
                 <h3>Pale ale</h3>
                 <p>Una rubia más amarga e intensa que la golden, pero también ligera y refrescante. Tiene más presencia de lúpulo.</p>
@@ -108,7 +120,7 @@
           </div>
           <div class="col-sm-6 col-md-3">
             <div class="thumbnail">
-              <img src="..." alt="...">
+              <img src="img/ipa.jpg" alt="...">
               <div class="caption">
                 <h3>IPA</h3>
                 <p>La sigla de India pale ale, es una versión de pale ale con altas dosis de flor de lúpulo. Tiene más cuerpo, es más amarga.</p>
@@ -118,7 +130,7 @@
           </div>
           <div class="col-sm-6 col-md-3">
             <div class="thumbnail">
-              <img src="..." alt="...">
+              <img src="img/scotch-ale.jpg" alt="...">
               <div class="caption">
                 <h3>Scotch ale</h3>
                 <p>Una cerveza rojiza y suave, con amargor bajo y notas de caramelo. Una versión un poco más dulce de su vecina irlandesa, la Irish red ale.</p>
@@ -130,7 +142,7 @@
         <div class="row">
           <div class="col-sm-6 col-md-3">
             <div class="thumbnail">
-              <img src="..." alt="...">
+              <img src="img/honey.jpg" alt="...">
               <div class="caption">
                 <h3>Honey</h3>
                 <p>Contiene miel, lo que la hace más dulce, pero muchas versiones también son más alcohólicas.</p>
@@ -140,7 +152,7 @@
           </div>
           <div class="col-sm-6 col-md-3">
             <div class="thumbnail">
-              <img src="..." alt="...">
+              <img src="img/porter.jpg" alt="...">
               <div class="caption">
                 <h3>Porter</h3>
                 <p>Una cerveza clásica inglesa, negra y liviana, con notas de chocolate y café.</p>
@@ -150,7 +162,7 @@
           </div>
           <div class="col-sm-6 col-md-3">
             <div class="thumbnail">
-              <img src="..." alt="...">
+              <img src="img/stout.jpg" alt="...">
               <div class="caption">
                 <h3>Stout</h3>
                 <p>Tiene versiones más dulces como la cream stout y versiones más amargas e intensas como la dry stout. Es negra, con mucho cuerpo y un carácter fuerte a café.</p>
