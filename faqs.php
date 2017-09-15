54b6d0f04e2fa9cd0ee8b07f874cabb2a9b84928
alvizu <robert.alvizu@gmail.com>
2017-09-14 21:07:11 -0300

    agregar faqs.php

---
+++ faqs.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Pulmahue</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://meyerweb.com/eric/tools/css/reset/reset.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <title>Pulmahue</title>
+  </head>
+  <body>
+    <div class="container">
+      <header>
+        <h1><a href="index.php"><img src="" alt="">Logo</a></h1>
+        <div class="login">
+          <ul>
+            <li><a href="login.php">Ingresar</a></li>
+            <li><a href="register.php">Registrarse</a></li>
+          </ul>
+        </div>
+      </header>
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Pulmahue</a>
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Quienes somos <span class="sr-only">(current)</span></a>
+              </li>
+              <li><a href="#">Promos</a>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Variedades <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Golden</a></li>
+                  <li><a href="#">Pale ale</a></li>
+                  <li><a href="#">IPA</a></li>
+                  <li><a href="#">Scotch ale</a></li>
+                  <li><a href="#">Honey</a></li>
+                  <li><a href="#">Porter</a></li>
+                  <li><a href="#">Stout</a></li>
+                  <li><a href="#">Imperial</a></li>
+                  <li role="separator" class="divider"></li>
+                  <li><a href="#">Nacionales</a></li>
+                  <li><a href="#">Importadas</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="navbar-form navbar-left">
+              <div class="form-group">
+                <input type="text" class="form-control" placeholder="Search">
+              </div>
+              <button type="submit" class="btn btn-default">Buscar</button>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="car.php">Carrito</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Usuario <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Cambiar correo</a></li>
+                  <li><a href="#">Cambiar contraseña</a></li>
+                  <li><a href="#">Cerrar sesion</a></li>
+                  <li role="separator" class="divider"></li>
+                  <li><a href="help.php">Ayuda</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+      </nav>
+      <h1>Preguntas frecuentes</h1>
+      <div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">¿Cuanto dura la cerveza sin abrir?</h3>
+  </div>
+  <div class="panel-body">
+    Se recomienda consumir antes de cumplirse los 6 meses de la producción de la misma.
+  </div>
+
+  <div class="panel-heading">
+    <h3 class="panel-title">¿Todas las cervezas tienen la misma graduación alcoholica?</h3>
+  </div>
+  <div class="panel-body">
+    No, depende del periodo de fermentación y las proporciones en los ingredientes de la misma.
+  </div>
+</div>
+    </div>
+  </body>
+</html>
