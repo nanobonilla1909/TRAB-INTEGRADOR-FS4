a98f2fddbb95b63c4866bfe28d2ae699757fc8b0
Nicolas Cordoba <nicolascordoba89@gmail.com>
2017-09-29 12:50:35 -0300

    funciones para registrar, guardar en Json y redirige al index

--- functions.php
+++ functions.php
@@ -32,6 +32,7 @@ function validateInformation($information) {
   $errorAvatar = $_FILES["avatar"]["error"];
   $nameAvatar = $_FILES["avatar"]["name"];
   $extention = pathinfo($nameAvatar, PATHINFO_EXTENSION);
+  $avatarSize = $_FILES["avatar"]["size"];
 
   if ($errorAvatar != UPLOAD_ERR_OK) {
     $errors["avatar"] = "Error al cargar la foto";
@@ -39,11 +40,42 @@ function validateInformation($information) {
   else if ($extention != "jpg" && $extention != "jpeg" && $extention != "png") {
     $errors["avatar"] = "El avatar debe tener extension JPG, JPEG o PNG";
   }
+  elseif ($avatarSize > 1572864) {
+    $errors["avatar"] = "El tamanio debe ser menor";
+  }
 
-  if (empty($information["terms"])) {
+  if (!isset($_POST["terms"])) {
     $errors["terms"] = "Debe ser mayor de edad y aceptar Terminos y condiciones";
   }
 
   return $errors;
 }
+
+function saveUser($user) {
+  $userJSON = json_encode($user);
+  file_put_contents("users.json", $userJSON . PHP_EOL, FILE_APPEND);
+}
+
+function doUser($data) {
+  return [
+    "name" => $data["name"],
+    "surname" => $data["surname"],
+    "email" => $data["email"],
+    "pass" => password_hash($data["pass"], PASSWORD_DEFAULT),
+  ];
+}
+
+function getAll() {
+  $file = file_get_contents("users.json");
+  $array = explode(PHP_EOL, $file);
+  array_pop($array);
+
+  $arrayFinal = [];
+  foreach ($array as $user) {
+    $arrayFinal[] = json_decode($user, true);
+  }
+
+  return $arrayFinal;
+}
+
 ?>
--- register.php
+++ register.php
@@ -1,17 +1,35 @@
 <?php
-	include("header.php");
-?>
-<?php
 require("functions.php");
 if ($_POST) {
+  // valido informacion
   $errors = validateInformation($_POST);
+  // si esta ok
+  if(count($errors) == 0){
+    // crea user
+    $user = doUser($_POST);
+    // lo guarda
+    saveUser($user);
+    // redirecciona al home
+    header("Location:index.php");exit;
+  }
 }
-?>
 
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Pulmahue</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://meyerweb.com/eric/tools/css/reset/reset.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/login.css">
+  </head>
   <body>
     <div class="container">
       <h1>Registrate</h1>
-      <form class="register" action="register_process.php" method="post" enctype="multipart/form-data">
+      <form class="register" action="register.php" method="post" enctype="multipart/form-data">
         <label>Nombre</label><br>
         <input class="form-control" type="text" name="name" value="<?=(!empty($_POST['name'])?$_POST['name']:"")?>">
         <?php if(!empty($errors['name'])) { ?><small><span class="error"><? echo $errors['name']?></span></small><br><?php } else { echo "<br>"; } ?>
@@ -22,18 +40,19 @@ if ($_POST) {
         <input class="form-control" type="text" name="email" value="<?=(!empty($_POST['email'])?$_POST['email']:"")?>">
         <?php if(!empty($errors['email'])) { ?><small><span class="error"><? echo $errors['email']?></span></small><br><?php } else { echo "<br>"; } ?>
         <label>Foto de Perfil</label><br>
-        <input type="file" name="avatar"><br>
+        <input type="file" name="avatar">
+        <?php if(isset($errors['avatar'])) { ?><small><span class="error"><? echo $errors['avatar']?></span></small><br><?php } else { echo "<br>"; } ?>
         <label>Contraseña</label><br>
         <input class="form-control" type="password" name="pass" value="<?=(!empty($_POST['pass'])?$_POST['pass']:"")?>">
         <?php if(!empty($errors['pass'])) { ?><small><span class="error"><? echo $errors['pass']?></span></small><br><?php } else { echo "<br>"; } ?>
         <label>Confirmar contraseña</label><br>
         <input class="form-control" type="password" name="pass-c" value="<?=(!empty($_POST['pass-c'])?$_POST['pass-c']:"")?>">
         <?php if(!empty($errors['pass-c'])) { ?><small><span class="error"><? echo $errors['pass-c']?></span></small><br><?php } else { echo "<br>"; } ?>
-        <input type="checkbox" name="terms" value=""> Soy mayor de 18 años y acepto los <strong>Terminos y condiciones</strong>
-        <?php if(!empty($errors['terms'])) { ?><br><small><span class="error"><? echo $errors['terms']?></span></small><br><?php } else { echo "<br>"; } ?>
+        <input type="checkbox" name="terms" value="1"> Soy mayor de 18 años y acepto los <strong>Terminos y condiciones</strong>
+        <?php if(isset($errors['terms'])) { ?><br><small><span class="error"><? echo $errors['terms']?></span></small><br><?php } else { echo "<br>"; } ?>
         <input type="checkbox" name="newsletter" value=""> Deseo recibir noticias del newsletter<br>
         <div class="submit">
-          <a href="register.php"><button type="submit" name="register">Registrarme</button></a>
+          <a href="register.php"><button type="submit" name="">Registrarme</button></a>
           <a href="index.php"><button type="submit" name="cancel">Cancelar</button></a>
         </div>
       </form>
